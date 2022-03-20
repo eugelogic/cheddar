6443135dffb3e497d5bc5c2e6900e5b85ec5e11d
Eugene Molari <eugelogic@gmail.com>
2022-03-20 18:34:21 +0000

    Fix POST method for Stores - take II

--- pages/api/stores/index.ts
+++ pages/api/stores/index.ts
@@ -3,37 +3,41 @@ import prisma from '@lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // hardcoded user id, to be replaced with value from authentication
+    const currentUser = { id: 9 }
+
     if (req.method === 'GET') {
         const stores = await prisma?.store.findMany()
         res.json(stores)
     } else if (req.method === 'POST') {
-        const { name, location, userId } = req.body
+        const { name, location } = req.body
+
         const schema = yup.object().shape({
             name: yup
                 .string()
                 .test('unique-name', 'Store name already exists.', async (value) => {
-                    const stores = await prisma?.store.findMany()
-                    const match = stores?.find((store) => store.name === value)
-                    return match === undefined
+                    const match = await prisma?.store.findFirst({
+                        where: {
+                            name: value,
+                            userId: currentUser.id,
+                        },
+                    })
+                    return match === null
                 })
                 .required(),
             location: yup.string().nullable(),
-            userId: yup.number().required(),
         })
-        const data = await schema.validate({ name, location, userId })
+        const data = await schema.validate({ name, location })
 
         const store = await prisma?.store.create({
             data: {
                 name: data.name,
                 location: data.location,
-                userId: data.userId,
+                userId: currentUser.id,
             },
         })
-        const response = {
-            store,
-            message: `New store successfully added.`,
-        }
-        res.status(201).json(response)
+
+        res.status(201).json(store)
     } else {
         res.status(405).json({ error: `Method ${req.method} not allowed.` })
     }
