86311ef4ab6e68e3f51ae1541ad82a6207e6b189
Eugene Molari <eugelogic@gmail.com>
2022-04-06 15:39:19 +0100

    Add error and auth handling to legacy endpoints

--- pages/api/lists/[id]/products.ts
+++ pages/api/lists/[id]/products.ts
@@ -1,28 +1,36 @@
-import prisma from '@lib/prisma'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@lib/prisma'
+import type { NextApiResponse } from 'next'
+import { handleAuth, handleErrors } from '@lib/api'
+import type { NextApiRequestWithUser } from '@lib/types'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (typeof prisma === 'undefined') {
-        res.status(500).json({ error: 'Internal server error.' })
-        return
-    }
-    const list = await prisma.list.findUnique({
-        where: {
-            id: Number(req.query.id),
-        },
+export default handleErrors(
+    handleAuth(async function handler(req: NextApiRequestWithUser, res: NextApiResponse) {
+        if (req.method === 'GET') {
+            const currentUser = req.user!
+            const list = await prisma.list.findFirst({
+                where: {
+                    AND: [
+                        { id: parseInt(`${req.query.id}`, 10) },
+                        {
+                            store: {
+                                userId: currentUser.id,
+                            },
+                        },
+                    ],
+                },
+            })
+            if (list === null || typeof list === 'undefined') {
+                res.status(404).json({ error: `List with id: ${req.query.id} does not exist.` })
+                return
+            }
+            const products = await prisma?.product.findMany({
+                where: {
+                    listId: list.id,
+                },
+            })
+            res.json(products)
+        } else {
+            res.status(405).json({ error: `Method ${req.method} not allowed.` })
+        }
     })
-    if (list === null || typeof list === 'undefined') {
-        res.status(404).json({ error: `List with id: ${req.query.id} does not exist.` })
-        return
-    }
-    if (req.method === 'GET') {
-        const products = await prisma?.product.findMany({
-            where: {
-                listId: list.id,
-            },
-        })
-        res.json(products)
-    } else {
-        res.status(405).json({ error: `Method ${req.method} not allowed.` })
-    }
-}
+)
--- pages/api/stores/[id]/lists.ts
+++ pages/api/stores/[id]/lists.ts
@@ -1,28 +1,33 @@
-import prisma from '@lib/prisma'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@lib/prisma'
+import type { NextApiResponse } from 'next'
+import { handleAuth, handleErrors } from '@lib/api'
+import type { NextApiRequestWithUser } from '@lib/types'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (typeof prisma === 'undefined') {
-        res.status(500).json({ error: 'Internal server error.' })
-        return
-    }
-    const store = await prisma.store.findUnique({
-        where: {
-            id: Number(req.query.id),
-        },
+export default handleErrors(
+    handleAuth(async function handler(req: NextApiRequestWithUser, res: NextApiResponse) {
+        if (req.method === 'GET') {
+            const currentUser = req.user!
+            const store = await prisma.store.findUnique({
+                where: {
+                    id: parseInt(`${req.query.id}`, 10),
+                },
+            })
+            if (store === null || typeof store === 'undefined') {
+                res.status(404).json({ error: `Store with id: ${req.query.id} does not exist.` })
+                return
+            }
+            if (currentUser.id !== store.userId) {
+                res.status(403).json({ error: 'User not authorised.' })
+                return
+            }
+            const lists = await prisma?.list.findMany({
+                where: {
+                    storeId: store.id,
+                },
+            })
+            res.json(lists)
+        } else {
+            res.status(405).json({ error: `Method ${req.method} not allowed.` })
+        }
     })
-    if (store === null || typeof store === 'undefined') {
-        res.status(404).json({ error: `Store with id: ${req.query.id} does not exist.` })
-        return
-    }
-    if (req.method === 'GET') {
-        const lists = await prisma?.list.findMany({
-            where: {
-                storeId: store.id,
-            },
-        })
-        res.json(lists)
-    } else {
-        res.status(405).json({ error: `Method ${req.method} not allowed.` })
-    }
-}
+)
